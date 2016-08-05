e0b928b66a4a2a32eca00b4caf5617f8941d4021
Akshay Kr <c13773@cerner.com>
2016-08-05 14:22:24 +0400

    Fix #7 - More Content, Quick Loading, Cosmetic Changes

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,11 +1,25 @@
 // Initialize your app
-var myApp = new Framework7();
+var myApp = new Framework7({
+    //swipePanel: 'left'
+});
 
 // Export selectors engine
 var $$ = Dom7;
 
-// Add view
-var mainView = myApp.addView('.view-main', {
+// Add views
+var view1 = myApp.addView('#view-1', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+var view2 = myApp.addView('#view-2', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+var view3 = myApp.addView('#view-3', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+var view4 = myApp.addView('#view-4', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
@@ -14,9 +28,9 @@ var mainView = myApp.addView('.view-main', {
 $$('.login-screen .list-button').on('click', function () {
         var username = $$('.login-screen input[name="username"]').val();
         var password = $$('.login-screen input[name="password"]').val();
-        myApp.alert('Login Accepted, ' + ' Tap OK to continue', function () {
+        //myApp.alert('Login Accepted, ' + ' Tap OK to continue', function () {
           myApp.closeModal('.login-screen');
-        });
+        //});
       });
  
 
@@ -43,65 +57,3 @@ $$(document).on('pageInit', function (e) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Callbacks to run specific code for specific pages, for example for About page:
-/*myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
-});
-
- Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}*/
-
-
- 
-/*$$('.panel-left').on('opened', function () {
-    myApp.alert('Left panel opened!');
-});
-$$('.panel-left').on('click', function () {
-    myApp.alert('Left panel is closing!');
-});*/ 
-   
